ae12dcd664ad0abb09649fe34a04262a945c0fad
olyagithub <olya.nicholls@hotmail.com>
2016-08-23 23:50:37 +0100

    st deviation

--- src/BusinessLogic/calculator.py
+++ src/BusinessLogic/calculator.py
@@ -1,23 +1,29 @@
-from audioop import avg
 from numpy import *
 
 
 class Calculator(object):
-
     @staticmethod
     def average_return(list_of_nums):
-        result = float(sum(list_of_nums) / len(list_of_nums))
-        return result
+        ar_result = float(sum(list_of_nums) / len(list_of_nums))
+        return ar_result
+
+    @staticmethod
+    def return_on_share_prices(list_of_prices):
+        rs_result = []
+        position = 0
+
+        while position < len(list_of_prices) - 1:
+            r = float((list_of_prices[position + 1] / list_of_prices[position]) - 1) #format(..., '.5f')  # !!!!! check
+            rs_result.append(r)
+            position += 1
+
+        return rs_result
+
+    @staticmethod
+    def annualise_as_percentage(num):
+        annualised_num = num * 12 * 100
+        return annualised_num
 
 
-    # @staticmethod
-    # def return_on_share_prices(list_of_prices):
 
 
-
-
-
-
-calc = Calculator()
-print(calc.average_return([12]))
-print(calc.average_return([1, 2, 4]) == mean([1, 2, 4]))
