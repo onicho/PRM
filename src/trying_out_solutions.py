b5ce030b0c9d89a6cd0117dbdab84bbbb1471143
olyagithub <olya.nicholls@hotmail.com>
2016-08-28 18:17:19 +0100

    eg procedure

--- src/trying_out_solutions.py
+++ src/trying_out_solutions.py
@@ -3,8 +3,8 @@ from collections import *
 
 c = Calculator()
 
-def order_by_erb(lst2d_shareprices, rf, mkt_prices):
 
+def order_by_erb(lst2d_shareprices, rf, mkt_prices):
     erbs_shares = {}
 
     for i in lst2d_shareprices:
@@ -16,15 +16,12 @@ def order_by_erb(lst2d_shareprices, rf, mkt_prices):
 
 
 def cut_off_rate(lst_shares, rf, mkt_prices):
-
     index = 0
     num_components = []
-
 
     for item in lst_shares:
 
         while index <= lst_shares.index(item):
-
             num_components.append(
                 float((c.annualise_as_percentage(c.average_return(c.return_on_share_prices(lst_shares[index])))) - rf) * \
                 float(c.beta(lst_shares[index], mkt_prices)) / \
@@ -38,9 +35,8 @@ def cut_off_rate(lst_shares, rf, mkt_prices):
     for item in lst_shares:
 
         while count <= lst_shares.index(item):
-
             denom_components.append(
-                float(pow(c.beta(lst_shares[count], mkt_prices),2)) / \
+                float(pow(c.beta(lst_shares[count], mkt_prices), 2)) / \
                 float(c.specific_risk(lst_shares[count], mkt_prices))
             )
             count += 1
@@ -48,7 +44,6 @@ def cut_off_rate(lst_shares, rf, mkt_prices):
     co_rates = []
 
     for share in lst_shares:
-
         n = len(lst_shares) - (lst_shares.index(share) + 1)
 
         num_element = num_components[:-n or None]  # to remove the last N elements of a list.
@@ -58,7 +53,7 @@ def cut_off_rate(lst_shares, rf, mkt_prices):
         var_mkt = c.total_risk(mkt_prices)
 
         cof = round(float((var_mkt * sum(num_element)) / \
-              (1 + var_mkt * (sum(den_element)))), 2)
+                          (1 + var_mkt * (sum(den_element)))), 2)
 
         co_rates.append(cof)
 
@@ -67,7 +62,7 @@ def cut_off_rate(lst_shares, rf, mkt_prices):
     for i in lst_shares:
         erbs_shares.append(c.erb(i, mkt_prices, rf))
 
-    cof_rate = 0
+    cof_rate = {}
     item_index = 0
     found = False
 
@@ -75,15 +70,48 @@ def cut_off_rate(lst_shares, rf, mkt_prices):
         if co_rates[item_index] <= erbs_shares[item_index + 1]:
             item_index += 1
         else:
-            cof_rate = co_rates[item_index]
+            cof_rate = {co_rates[item_index]: item_index}
             found = True
 
-    return cof_rate
+    return cof_rate, co_rates, len(lst_shares)
 
 
+def cor_filter_shares_portf(lst_shares, cof_index):
+    n = (list(cof_index.values()))[0] + 1
+
+    filtered_shares = lst_shares[: n]
+
+    return filtered_shares
 
 
+def unadjusted_weights(lst_shares, mkt_prices, cof_index, rf):
+    cof = list(cof_index.keys())[0]
 
+    unadj_weights = []
+
+    for i in lst_shares:
+        w = (
+              float((c.beta(i, mkt_prices) / c.specific_risk(i, mkt_prices))) *
+
+              (float(c.erb(i,mkt_prices,rf)) - float(cof))
+        )
+        unadj_weights.append(w)
+
+
+    return unadj_weights
+
+
+def normalised_weights(unadj_weights):
+
+    sum_of_weights = sum(unadj_weights).round(6)
+
+    norm_weights = []
+
+    for i in unadj_weights:
+        norm_weights.append(i/sum_of_weights)
+
+
+    return norm_weights
 
 
 
@@ -134,13 +162,27 @@ NGprices = [float(i) for i in array4]
 
 testlist = order_by_erb([NGprices, AMLprices, ERMprices, CGLprices], 1.5, MKTprices)
 
-
 test = cut_off_rate(testlist, 1.5, MKTprices)
 
-# for i in test:
-#     print(i)
+for i in test:
+    print(i)
 
-print(test)
+# print(test)
+
+
+
+test2 = cor_filter_shares_portf(testlist, {7.35: 2})
+
+for i in test2:
+    print(i)
+
+test3 = unadjusted_weights(test2,MKTprices,{7.35: 2},1.5)
+print(test3)
+
+
+test4 = normalised_weights(test3)
+
+print(test4)
 
 
 #############################################
