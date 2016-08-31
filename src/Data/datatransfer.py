9ffbc8f0fe05de0e2f9b457c3dedc429837285fa
ONicholls <olya.nicholls@hotmail.com>
2016-08-31 21:12:33 +0100

    updating prices

--- src/Data/datatransfer.py
+++ src/Data/datatransfer.py
@@ -4,44 +4,31 @@ from pprint import pprint
 lonExt = '.L'
 
 from sqlalchemy import *
+
 URL = 'mssql+pyodbc://GMT04999:1433/PRM_Lite?driver=SQL+Server'
 
-engine = create_engine(URL, echo = True)
+engine = create_engine(URL, echo=True)
 
-conn = engine.connect()
+conn1 = engine.connect()
 
 sharesQuery = text(
-        "SELECT distinct epic "
-        "FROM [dbo].[SHARE]"
+    "SELECT distinct epic "
+    "FROM [dbo].[SHARE]"
 )
 
-metadata = MetaData()
-
-orders = Table('SHARE_CALENDAR', metadata,
-    Column('EPIC', String),
-    Column('CALENDAR_DATE', Date),
-    Column('PRICE', Integer)
-)
-
-po = conn.execute(sharesQuery)
+po = conn1.execute(sharesQuery)
 
 for epicCode in po:
     ShareObject = Share(epicCode['epic'] + lonExt)
 
     print(ShareObject.get_price())
 
-    prices = [{p['Date']:p['Close']} for p in ShareObject.get_historical('2009-01-01', '2009-01-15')]
+    prices = [{p['Date']: p['Close']} for p in ShareObject.get_historical('2009-01-01', '2009-01-15')]
     for priceDict in prices:
+        conn = engine.connect()
         price = list(priceDict.items())
-        # update_blah = (
-        # orders.update.\
-        #     where(and_(orders.c.CALENDAR_DATE == price[0],
-        #                orders.c.EPIC == epicCode['epic'])).
-        #     values(PRICE=price[1])
-        # )
 
-        updateQuery = "update [dbo].[SHARE_CALENDAR] set price = " + price[1] + " where CALENDAR_DATE = '" + price[0] + "' AND epic = '" + epicCode['epic'] +"'"
+        updateQuery = "update [dbo].[SHARE_CALENDAR] set price = " + price[0][1] + " where CALENDAR_DATE = '" + \
+                      price[0][0] + "' AND epic = '" + epicCode['epic'] + "'"
 
-        print(updateQuery)
-
-        print(conn.execute(updateQuery))
+        conn.execute(updateQuery)
