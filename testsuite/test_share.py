53ceeb3a6ee391e111092ac71e2bc63b7ce50f11
olyagithub <olya.nicholls@hotmail.com>
2016-09-07 01:07:28 +0100

    share tests

--- testsuite/test_share.py
+++ testsuite/test_share.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from logic.share import Share
+from logic.share import *
 
 
 class TestShare(TestCase):
@@ -40,6 +40,57 @@ class TestShare(TestCase):
         self.assertNotEqual(s, s1)
 
 
+class TestShareFactory(TestCase):
+
+    def test_create_success(self):
+        """Test that a new Share object called 'NG' is created and that Share's
+        assigned prices are equal to the list of historica prices copied from
+        PRM database for the same period
+        """
+        factory = ShareFactory.create("NG", "2009-01-01", "2009-12-31")
+        p = [
+                574.407000000,
+                555.349000000,
+                474.684000000,
+                500.390000000,
+                530.529000000,
+                485.321000000,
+                495.071000000,
+                525.210000000,
+                535.404000000,
+                537.620000000,
+                585.487000000,
+                601.886000000
+        ]
+        self.assertEqual(factory.name, "NG")
+        self.assertEqual(factory.prices, p)
+
+    def test_create_priceless(self):
+        """
+        Test that if the end date precedes the start date for the prices
+        period, the database does not return any results and the Share object is
+        assigned an empty list of prices.
+        """
+        factory = ShareFactory.create("LLOY", "2009-01-01", "2000-01-01")
+        self.assertEqual(factory.name, "LLOY")
+        self.assertEqual(factory.prices, [])
+
+    def test_create_type(self):
+        """
+        Test a new object of the type Share is returned
+        """
+        factory = ShareFactory.create("TSCO", "2009-01-01", "2014-01-05")
+        self.assertEqual(type(factory), Share)
+
+    def test_create_fail(self):
+        """
+        Test that even if a new object Share is created with an invalid name, no
+        prices are assigned to the Share object because the database cannot
+        retrieve historical prices for invalid share name.
+        """
+        factory = ShareFactory.create("", "2009-01-01", "2014-01-05")
+        self.assertEqual(factory.name, "")
+        self.assertFalse(len(factory.prices) > 0)
 
 
 
