3670d11a4f94d6ef9dbabaf11020f56349778d07
olyagithub <olya.nicholls@hotmail.com>
2016-09-13 01:21:28 +0100

    user input tests completed

--- src/Presentation/input.py
+++ src/Presentation/input.py
@@ -1,11 +1,13 @@
 import pyodbc
 from datetime import *
 from colorama import *
+import re
 
 
 def get_tickers():
     """
-    Gets user input, i.e. EPIC codes (tickers) of the shares to be processed
+    Prompts user input, and accepts EPIC codes (tickers) of the shares to be
+    processed
 
     :return: a list of share EPIC codes
     :rtype: list[str]
@@ -17,41 +19,34 @@ def get_tickers():
     sentinel = "0"
 
     while not correct and count < 10:
-        #print()
+        print()
         ticker = input(Fore.LIGHTGREEN_EX +
-                       'Please enter an EPIC code of a share( or 0 to finish):')
-        print('\033[30m')
+                       'Please enter an EPIC code of a share( or 0 to finish):'
+                       + '\033[30m')
         ticker = ticker.upper().replace(" ", "")
-        try:
-            if valid(ticker) and ticker not in tickers:
-                tickers.append(ticker)
-                count += 1
-                if count == 10:
-                    print(
-                        "You have reached the maximum number of shares for "
-                        "processing by the PRM system")
-                    return tickers
 
-            elif ticker == sentinel:
+        if valid_epic(ticker) and ticker not in tickers:
+            tickers.append(ticker)
+            count += 1
+            if count == 10:
+                print(
+                    "You have reached the maximum number of shares for "
+                    "processing by the PRM system")
                 return tickers
 
-            else:
-                print()
-                print('\033[31m' + "Invalid entry.")
-                print('\033[30m')
-                print("Possible reasons: ")
-                print("     * EPIC has already been entered")
-                print("     * EPIC is spelt incorrectly.")
-                print("     * EPIC is not in FTSE100 or not in PRM database")
-                raise ValueError
+        elif ticker == sentinel:
+            return tickers
 
-        except ValueError:
+        else:
             print()
-            print("Try again or enter 0 (zero) to stop input")
-            print()
+            print('\033[31m' + "Invalid entry." + '\033[30m')
+            print("Possible reasons: ")
+            print("     * EPIC has already been entered")
+            print("     * EPIC is spelt incorrectly.")
+            print("     * EPIC is not in FTSE100 or not in the PRM database")
 
 
-def valid(ticker):
+def valid_epic(ticker):
     """
     Checks that entered EPIC copde exists in the PRM database
 
@@ -72,98 +67,134 @@ def valid(ticker):
         return False
 
 
-print(get_tickers())
+def get_period():
+    """
+    Prompts user input to for the start date and the end date of the time period
+    that is used by class ShareFactory to create shares.
+
+    :return: start and end dates of the time period in the 'yyyy-mm-dd' format
+    :rtype: list[str]
+    """
+    today = date.today()
+    days = timedelta(days=31)
+    delta = today - days
+
+    period = []
+
+    print()
+    print("Enter start and end dates in the following format" + '\033[4m' +
+          " yyyy-mm-dd " + '\033[30m')
+
+    correct = False
+
+    while not correct:
+        print()
+
+        start = str(input(Fore.LIGHTGREEN_EX + "FROM:  " + '\033[30m')). \
+            replace(" ", "")
+
+        if valid_date(start):
+
+            if datetime.strptime(start, "%Y-%m-%d").date() <= delta:
+                period.append(start)
+                break
+
+            else:
+                print("Invalid entry. Possible reasons: ")
+                print("The date appears to be after " +
+                      delta.strftime("%Y-%m-%d") +
+                      " Enter an earlier date.")
+
+        else:
+            print("Invalid entry. Please check the format of your date")
+            print("The date should be between 2009-01-01 and " +
+                  delta.strftime("%Y-%m-%d"))
+
+    while not correct:
+        print()
+        end = str(input(Fore.LIGHTGREEN_EX + "TO:  " + '\033[30m')). \
+            replace(" ", "")
+
+        d = datetime.strptime(period[0],"%Y-%m-%d").date()
+
+        if valid_date(end):
+
+            if datetime.strptime(end, "%Y-%m-%d").date() >= d + days:
+
+                period.append(end)
+                correct = True
+
+            else:
+                print("The earliest end date can be " + (d + days).
+                      strftime("%Y-%m-%d"))
+
+                print("The current version of the system works with monthly "
+                      "share prices")
+
+        else:
+            print("Invalid entry. Please check the format of your date")
+            print("The date should be between " +
+                  (d + days).strftime("%Y-%m-%d") + " and " + str(date.today() -
+                                                                  timedelta(1)))
+
+    return period
 
 
+def valid_date(dt):
+    """
+    Checks that entered date exists in the calendar of the PRM database
 
-# def get_period_from_user(self):
-#     today = date.today()
-#     calendar_period = []
-#     print()
-#     print(
-#         "please enter the assessment period in the following format yyyy-mm-dd......")  # RE-WORD
-#     print()
-#
-#     input_correct = False
-#
-#     while not input_correct:
-#         print()
-#         start_date = str(input("FROM:  ")).replace(" ", "")  # DO ERROR CATCHING
-#
-#         if self.valid_date(start_date):
-#             if datetime.strptime(start_date, "%Y-%m-%d").date() < today:
-#                 calendar_period.append(start_date)
-#                 # input_correct = True
-#                 break
-#
-#             else:
-#                 print(
-#                     "The date you entered appears to be today's date or the one in the future")
-#                 print(
-#                     "No stock prices are available yet. Please enter an earlier date.")
-#
-#         else:
-#             print("Invalid entry. Please check the format of your date")
-#             print("The time period should be between 2009-01-01 and " + str(
-#                 today))
-#
-#     while not input_correct:
-#         print()
-#         end_date = str(input("TO:  ")).replace(" ", "")  # DO ERROR CATCHING
-#
-#         if self.valid_date(end_date):
-#             if datetime.strptime(calendar_period[0], "%Y-%m-%d").date() < \
-#                     datetime.strptime(end_date, "%Y-%m-%d").date() < today:
-#                 calendar_period.append(end_date)
-#                 input_correct = True
-#             else:
-#                 print(
-#                     "Dates inconsistency. The end date cannot precede the start date. ")
-#                 print("Also the end date cannot be a future or today's date.")
-#
-#         else:
-#             print("Invalid entry. Please check the format of your date")
-#             print("The time period should be between 2009-01-01 and " + str(
-#                 date.today()))
-#     return calendar_period
-#
-#
-#
-#
-#
-#
-#
-#
-# def valid_date(date):
-#     cnxn = pyodbc.connect(
-#         'driver={SQL Server};server=localhost;database=PRM;Integrated Security=True')
-#     cursor = cnxn.cursor()
-#     cursor.execute("SELECT distinct CALENDAR_DATE FROM [dbo].[CALENDAR]")
-#     if date in [date[0] for date in cursor.fetchall()]:
-#         return True
-#     else:
-#         return False
-#
-#
-# def risk_free_rate():
-#     input_correct = False
-#
-#     while not input_correct:
-#
-#         try:
-#             rfr = float(
-#                 (input("Risk Free Rate as percentage % :  ")).replace(" ", ""))
-#             if 0 < rfr < 100:
-#                 input_correct = True
-#             else:
-#                 print(
-#                     "The risk free percentage is a number that is more than 0% but less than 100%. Try again")
-#
-#         except ValueError:
-#             print("The risk free rate must be a number. Please try again")
-#
-#     return rfr
+    :param dt: calendar dtae
+    :type dt: str ('yyyy-mm-dd')
+    :return: boolean
+    """
+
+    cnxn = pyodbc.connect(
+        'driver={SQL Server};server=localhost;database=PRM;'
+        'Integrated Security=True')
+    cursor = cnxn.cursor()
+
+    cursor.execute("SELECT distinct CALENDAR_DATE FROM [dbo].[CALENDAR]")
+
+    if dt in [str(d[0]) for d in cursor.fetchall()]:
+
+        return True
+
+    else:
+        return False
 
 
+def get_rfr():
+    """
+    Prompts user input - the risk free rate for calculations
+    :return: float
+    """
 
+    input_correct = False
+    risk = 0
+
+    while not input_correct:
+
+        try:
+            print()
+            rfr = float(
+                (input(Fore.LIGHTGREEN_EX + "Risk Free Rate as percentage:  " +
+                       '\033[30m')).replace(" ", ""))
+
+            if 0 < rfr < 100:
+
+                risk = rfr
+                input_correct = True
+
+            else:
+                print(
+                    "The percentage value should be a number that is higher "
+                    "than 0% but less than 100%" )
+                print("Please try again")
+
+        except ValueError:
+
+            print("Please enter a valid number")
+
+    return risk
 
--- src/trying_out_solutions.py
+++ src/trying_out_solutions.py
@@ -1,16 +1,16 @@
-from logic.share import ShareFactory
-from logic.portfolio import *
-import pyodbc
-from yahoo_finance import *
-
-cnxn = pyodbc.connect(
-    'driver={SQL Server};server=localhost;database=PRM;'
-    'Integrated Security=True')
-cursor = cnxn.cursor()
-
-cursor.execute("SELECT distinct CALENDAR_DATE FROM [dbo].[CALENDAR]")
-
-x = [str(d[0]) for d in cursor.fetchall()]
-
-
-print('2016-05-12' in x)
+# from logic.share import ShareFactory
+# from logic.portfolio import *
+# import pyodbc
+# from yahoo_finance import *
+#
+# cnxn = pyodbc.connect(
+#     'driver={SQL Server};server=localhost;database=PRM;'
+#     'Integrated Security=True')
+# cursor = cnxn.cursor()
+#
+# cursor.execute("SELECT distinct CALENDAR_DATE FROM [dbo].[CALENDAR]")
+#
+# x = [str(d[0]) for d in cursor.fetchall()]
+#
+#
+# print('2016-05-12' in x)
